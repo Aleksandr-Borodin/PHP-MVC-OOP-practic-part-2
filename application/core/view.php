13340ada1d50ea97ca8f526fa1ac1a7f7741876f
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-04-19 14:33:32 +0300

    Добавлены комментарии, которые обьясняют, как формируется внешний вид страницы

--- application/core/view.php
+++ application/core/view.php
@@ -1,22 +1,17 @@
 <?php
-
-class View
-{
-    //public $template_view; // здесь можно указать общий вид по умолчанию.
-    /**
-     * @param $content_view - виды отображающие контент страниц;
-     * @param $template_view - общий для всех страниц шаблон;
-     * @param null $data -  массив, содержащий элементы контента страницы. Обычно заполняется в модели.
-     */
-    function generate($content_view, $template_view, $data = null)
-    {
-
-        if(is_array($data)) {
-            // преобразуем элементы массива в переменные
-            extract($data);
-        }
-
-
-        include 'application/views/' . $template_view;
+// Базовый класс для отображения, принимает параметры, подключает файл;
+class View {
+  //public $template_view; // здесь можно указать общий вид по умолчанию.
+  /**
+  $content_view - виды отображающие контент страниц;
+  $template_view - общий для всех страниц шаблон;
+  null $data -  массив, содержащий элементы контента страницы. Обычно заполняется в модели.
+  */
+  function generate($content_view, $template_view, $data = null) {
+    if(is_array($data)) {
+      // преобразуем элементы массива в переменные
+      extract($data);
     }
+    include 'application/views/' . $template_view;
+  }
 }
