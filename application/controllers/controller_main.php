036b92fbb7792d3f565eb0b348ba500396deb389
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-04-19 14:22:29 +0300

    Отформатирован код

--- application/controllers/controller_main.php
+++ application/controllers/controller_main.php
@@ -1,16 +1,15 @@
 <?php
-
-class Controller_Main extends Controller
-{
-    function action_index()
-    {
-        $this->view->generate('main_view.php', 'template_view.php',
-            array(
-                'title' => 'Главная страница',
-                'is_photo_slider' => true,
-                'is_slider' => true,
-                'is_right_sidebar' => true
-            )
-        );
-    }
+// Класс формирующий главную страницу;
+class Controller_Main extends Controller {
+  function action_index() {
+    // $this->view = core/view;
+    $this->view->generate('main_view.php', 'template_view.php',
+      array(
+        'title' => 'Главная страница',
+        'is_photo_slider' => true,
+        'is_slider' => true,
+        'is_right_sidebar' => true
+      )
+    );
+  }
 }
