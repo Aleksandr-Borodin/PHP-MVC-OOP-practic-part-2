d62c411bb3267479ca8a44bc56c7ed6d53936c21
Aleksandr-Borodin <aleksandrov477@gmail.com>
2017-04-19 14:25:28 +0300

    Добавлен параметр кодировки при подключении к БД + форматирование

--- application/core/model.php
+++ application/core/model.php
@@ -1,18 +1,9 @@
 <?php
 
-abstract class Model
-{
-    /**
-     * @var PDO
-     */
-    protected $_pdo;
-
-    public function __construct(){
-        $this->_pdo = new PDO('mysql:dbname=shop-test;host=localhost', 'root', '');
-    }
-
-
-    public function get_data()
-    {
-    }
+abstract class Model {
+  protected $_pdo;
+  public function __construct(){
+    $this->_pdo = new PDO('mysql:dbname=shop-test;host=localhost;charset=utf8', 'root', '');
+  }
+  public function get_data() {}
 }
